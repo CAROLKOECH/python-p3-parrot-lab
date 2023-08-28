b0b6379f491b7907be46cea2eecc86857d1b5ec8
CAROLKOECH <koechc44@gmail.com>
2023-08-28 23:58:41 +0300

    done

--- lib/testing/parrot_test.py
+++ lib/testing/parrot_test.py
@@ -12,11 +12,11 @@ class TestParrot:
         sys.stdout = captured_out
         parrot("Hello!")
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Hello!\n")
+        assert captured_out.getvalue() == "Hello!\n"
 
     def test_returns_argument(self):
         '''returns the argument passed to it.'''
-        assert(parrot("Hello!") == "Hello!")
+        assert parrot("Hello!") == "Hello!"
 
     def test_prints_squawk_by_default(self):
         '''prints "Squawk!" if no argument is passed.'''
@@ -24,8 +24,8 @@ class TestParrot:
         sys.stdout = captured_out
         parrot()
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Squawk!\n")
+        assert captured_out.getvalue() == "Squawk!\n"
 
     def test_returns_squawk_by_default(self):
         '''returns "Squawk!" if no argument is passed.'''
-        assert(parrot() == "Squawk!")
+        assert parrot() == "Squawk!"
